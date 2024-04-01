926f0e5cdc53cc24e9dc6bc0350f4ded0bfd8d19
poonam <mehrapoonam379@gmail.com>
2024-04-01 10:51:08 +0530

    commit by poonam

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,71 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  
+  username: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  profile: {
+    type: String,
+    trim: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  isBlock: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "vendor"],
+    required: true,
+    default: "customer",
+  },
+  location: [
+    {
+      country: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      state: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      city: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      pincode: {
+        type: Number,
+        required: true,
+        trim: true,
+      },
+    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("user", userSchema);
 
-export default User;
+export default User;
