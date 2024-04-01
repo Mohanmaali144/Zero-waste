53f81879bc867c2b9b50b983ce3a22cf95fcb273
poonam <mehrapoonam379@gmail.com>
2024-04-01 10:52:53 +0530

    Committing local changes before pulling from remote

--- src/db/doConfig.js
+++ src/db/doConfig.js
@@ -1,7 +1,7 @@
  import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-// db connect using uri  because in deploye it is provide to link of http link o f database 
+// db connect using uri  because in deploye it is provide to link of http link of database 
 const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
