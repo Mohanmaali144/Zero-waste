407bde59622a740dc92a6fc08d8bb8b0d27761ab
mohan_oon <mohanmaali143@gmail.com>
2024-04-23 10:40:32 +0530

    Changrs in product controller

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { body, param } from "express-validator";
+import { upload } from "../middlewares/multer.middleware.js";
 
-import { addAllProduct, fetchProductByCategory, fetchProductById, fetchProductByName, fetchProductByPrice, productList, removeProductById, removeProductByName, updateProduct, addProduct} from "../controllers/product.controller.js";
+import { addAllProduct, fetchProductByCategory, fetchProductById, fetchProductByName, fetchProductByPrice, productList, removeProductById, removeProductByName, updateProduct, addProduct } from "../controllers/product.controller.js";
 
 const productRouter = express.Router();
+
+
 productRouter.post("/addAllProduct",
     body("productName", "product name is required").notEmpty(),
     body("description", "description is required").notEmpty(),
@@ -24,7 +27,11 @@ productRouter.post("/addAllProduct",
     body("commission", "commission is required").isNumeric(),
     addAllProduct);
 
-    productRouter.post("/addProduct",
+productRouter.post("/addProduct", 
+upload.fields([
+    { name: 'thumbnail', maxCount: 1 }, // For single thumbnail upload
+    { name: 'images', maxCount: 5 }     // For multiple images (up to 5) upload
+]),
     body("productName", "product name is required").notEmpty(),
     body("description", "description is required").notEmpty(),
     body("price", "price is required").notEmpty(),
@@ -44,33 +51,33 @@ productRouter.post("/addAllProduct",
     body("commission", "commission is required").isNumeric(),
     addProduct);
 
-    productRouter.get("/productList", productList);
+productRouter.get("/productList", productList);
 
-    productRouter.get("/fetchProductById/:id",
+productRouter.get("/fetchProductById/:id",
     param("id", "product id is required").notEmpty(),
     fetchProductById);
 
-    productRouter.get("/fetchProductByName/:name",
+productRouter.get("/fetchProductByName/:name",
     param("name", "product name is required").notEmpty(),
     fetchProductByName);
 
-    productRouter.get("/fetchProductByCategory/:category",
+productRouter.get("/fetchProductByCategory/:category",
     param("category", "product category is required").notEmpty(),
     fetchProductByCategory);
 
-    productRouter.get("/fetchProductByPrice/:price",
+productRouter.get("/fetchProductByPrice/:price",
     param("price", "product price is required").notEmpty(),
     fetchProductByPrice);
 
-    productRouter.delete("/removeProductById/:id",
+productRouter.delete("/removeProductById/:id",
     param("id", "product id is required").notEmpty(),
     removeProductById);
 
-    productRouter.delete("/removeProductByName/:name",
+productRouter.delete("/removeProductByName/:name",
     param("name", "product name is required").notEmpty(),
     removeProductByName);
 
-    productRouter.put("/updateProduct", updateProduct);
+productRouter.put("/updateProduct", updateProduct);
 
 // router.get("/searchProduct",searchProduct);
 // router.delete("/removeImage",removeImage);
